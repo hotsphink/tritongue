011d46020ba91c25fcf51e93edf254c5be6f1b0a
Steve Fink <sfink@mozilla.com>
2024-09-23 13:43:00 -0700

    ImLook for config file in $XDG_CONFIG_DIR

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,46 @@
-use anyhow;
+use anyhow::bail;
+use dirs;
 use trinity::BotConfig;
+use std::path::{Path, PathBuf};
+
+// If a path is given, return that if it exists else panic. If a path is not
+// given, look in $XDG_CONFIG_DIR/tritongue and return that if it exists else
+// return None.
+//
+// Yes, this suffers from TOCTOU. (But it'll error out later.)
+fn config_dir_filename(path: Option<String>, default: &str) -> Result<Option<PathBuf>, anyhow::Error> {
+    if let Some(path) = path {
+        if Path::new(&path).is_file() {
+            Ok(Some(PathBuf::from(&path)))
+        } else {
+            bail!("config file {} not found", path)
+        }
+    } else {
+        let Some(config_root) = dirs::config_dir() else { bail!("no config_dir directory found") };
+        let config_dir = config_root.join("tritongue");
+        let rel = config_dir.join(default);
+        if rel.is_file() {
+            Ok(rel.to_str().map(|s| PathBuf::from(s)))
+        } else {
+            Ok(None)
+        }
+    }
+}
 
 async fn real_main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    let config_path = std::env::args().nth(1);
 
-    tracing::debug!("parsing config...");
-    // First check for a config file, then fallback to env if none found.
-    let config = BotConfig::from_config(config_path).or_else(|_| BotConfig::from_env())?;
+    // This really shouldn't be checked if path is given.
+    let config_param = std::env::args().nth(1);
+    let Ok(filename) = config_dir_filename(config_param, "config.toml")
+        else { anyhow::bail!("error looking for config file") }; // FIXME: Propagate actual error.
+    // Check for a config file, then fallback to env if none found.
+    let config = if let Some(config_path) = filename {
+        tracing::debug!("parsing config {:?}...", config_path.to_string_lossy());
+        BotConfig::from_config(Some(String::from(config_path.to_string_lossy())))?
+    } else {
+        BotConfig::from_env()?
+    };
 
     tracing::debug!("creating client...");
     trinity::run(config).await
