5167f2ad6ce965e9b41d90145a4043b19de8ae83
Steve Fink <sphink@gmail.com>
2025-02-03 12:29:08 -0800

    Loop, continuing after errors. (Warning: this will spin on some errors! Maybe I should restrict to timeouts?)

--- src/lib.rs
+++ src/lib.rs
@@ -1147,15 +1147,17 @@ pub async fn run(config: BotConfig) -> anyhow::Result<()> {
     client.add_event_handler(on_stripped_state_member);
     client.add_event_handler(on_verification_request);
 
-    tokio::select! {
-        _ = handle_signals() => {
-            println!("Signal received");
-            // Exit :)
-        }
+    loop {
+        tokio::select! {
+            _ = handle_signals() => {
+                println!("Signal received");
+                break
+                // Exit :)
+            }
 
-        Err(err) = client.sync(sync_settings) => {
-            println!("Error caught");
-            bail!(err);
+            Err(err) = client.sync(sync_settings.clone()) => {
+                println!("Error caught: {}", err);
+            }
         }
     }
 
